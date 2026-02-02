a1558eb6c74aeff0cea9a1d41b5a43b8f0d9d235
teerath <teerathjammar8@gmail.com>
2026-02-02 22:27:22 +0530

    Add chai menu, types, tuples, and enums to functions.ts

--- src/functions.ts
+++ src/functions.ts
@@ -3,3 +3,59 @@ function makeChai(name: string, cups: number) {
 }
 
 makeChai("Ginger", 10);
+
+const chaiFlavours: string[] = ["Masala", "Ginger"];
+const chaiPrice: number[] = [12, 15];
+const ratings: Array<number> = [4.5, 4.0];
+
+type Chai = {
+  name: string;
+  price: number
+}
+
+const menu: Array<Chai> = [
+  {name: "Ginger", price: 15}
+  {name: "Masala", price: 30}
+]
+
+const availableStore: readonly string[] = ["Mumbai", "Nagpur"]
+
+// tuple
+const table: number[][] = [
+  [1, 2, 3],
+  [4, 5, 6]
+]
+
+let chaiTuple: [string, number]
+chaiTuple = ["Masala", 30]
+
+let userInfo: [string, number, boolean]
+userInfo = ["XYZ", 25, true]
+
+// readonly tuple
+const location: readonly [number, number]  = [1947.44, 46473.10]
+
+//named tuple
+const userLocation: [altitude: number, longitude: number] = [2634.33, 173.44]
+
+//enums
+enum Cupsize  {
+  SMALL,
+  LARGE,
+  MEDIUM
+}
+
+const cupSize = Cupsize.MEDIUM
+
+enum AvailableSize {
+  SMALL = "small",
+  MEDIUM = "medium",
+  LARGE = "large"
+}
+
+function chaiOrder(size:AvailableSize) {
+  console.log(size);
+}
+
+chaiOrder(AvailableSize.MEDIUM)
+
